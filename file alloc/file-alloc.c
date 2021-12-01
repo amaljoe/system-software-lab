2f1e79cf5ea889912045b66ea11f4ce790c3a13c
Amal Joe R S <amaljoe88@gmail.com>
2021-12-01 21:51:35 +0530

    file alloc progress

--- file alloc/file-alloc.c
+++ file alloc/file-alloc.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
 struct file
 {
     char name[10];
     int location[50];
     int size;
+    int starting_address;
 } files[50];
 
+int n;
+
 void sequential();
+int search(char[]);
 
 int main()
 {
@@ -17,16 +22,37 @@ int main()
 
 void sequential()
 {
-    int n;
     printf("Enter the number of files:\n");
     scanf("%d", &n);
     for (int i = 0; i < n; i++)
     {
         printf("Enter the name of file %d:\n", i + 1);
         scanf("%s", files[i].name);
+        printf("Enter the size of file %d:\n", i + 1);
+        scanf("%s", files[i].size);
+        printf("Enter the starting index of file %d:\n", i + 1);
+        scanf("%s", files[i].starting_address);
     }
+    char key[10];
+    printf("Enter the name of file to be searched:\n");
+    scanf("%s", key);
+    int index = search(key);
+    if (index == -1)
+    {
+        printf("File not found\n");
+        return;
+    }
+}
+
+// search for a filename in a list of files, return file index or -1 if not found
+int search(char key[])
+{
     for (int i = 0; i < n; i++)
     {
-        printf("%s\n", files[i].name);
+        if (strcmp(key, files[i].name) == 0)
+        {
+            return i;
+        }
     }
+    return -1;
 }
